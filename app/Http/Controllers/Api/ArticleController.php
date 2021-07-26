ab262b401927343b794df76ae3549ed5f540c8b6
Deivy <dpaul_20@hotmail.com>
2021-07-26 20:17:49 -0300

    Aprendible 10 - Probando errores de validación en formato JSON

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -28,8 +28,14 @@ class ArticleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) : ArticleResource
     {
+        $request->validate([
+            'data.attributes.title' => ['required', 'min:4'],
+            'data.attributes.slug' => ['required'],
+            'data.attributes.content' => ['required'],
+        ]);
+
         $article = Article::create([
             'title' => $request->input('data.attributes.title'),
             'slug' => $request->input('data.attributes.slug'),
--- tests/Feature/Articles/CreateArticleTest.php
+++ tests/Feature/Articles/CreateArticleTest.php
@@ -14,8 +14,6 @@ class CreateArticleTest extends TestCase
     /** @test */
     public function can_create_articles()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->postJson(route('api.v1.articles.store'), [
             'data' => [
                 'type' => 'articles',
@@ -51,4 +49,70 @@ class CreateArticleTest extends TestCase
             ]
         ]);
     }
+
+     /** @test */
+    public function title_is_required()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del artículo'
+                ]
+            ]
+        ]);
+
+        $response->assertJsonValidationErrors('data.attributes.title');
+
+    }
+
+     /** @test */
+     public function slug_is_required()
+     {
+         $response = $this->postJson(route('api.v1.articles.store'), [
+             'data' => [
+                 'type' => 'articles',
+                 'attributes' => [
+                     'title' => 'Nuevo Articulo',
+                     'content' => 'Contenido del artículo'
+                 ]
+             ]
+         ]);
+
+         $response->assertJsonValidationErrors('data.attributes.slug');
+     }
+
+     /** @test */
+    public function title_must_be_at_least_4_characters()
+    {
+        $response = $this->postJson(route('api.v1.articles.store'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'Nue',
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del artículo'
+                ]
+            ]
+        ]);
+
+        $response->assertJsonValidationErrors('data.attributes.title');
+    }
+
+     /** @test */
+     public function content_is_required()
+     {
+         $response = $this->postJson(route('api.v1.articles.store'), [
+             'data' => [
+                 'type' => 'articles',
+                 'attributes' => [
+                     'title' => 'Nuevo Articulo',
+                     'slug' => 'nuevo-articulo'
+                 ]
+             ]
+         ]);
+
+         $response->assertJsonValidationErrors('data.attributes.content');
+     }
 }
